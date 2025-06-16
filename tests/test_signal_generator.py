b5cada72b1f2d58d9a544dea4ef8a4fa2c581181
nothing14yyx <63762380+nothing14yyx@users.noreply.github.com>
2025-06-16 11:43:49 +0800

    Update voting and position logic with AI weight

--- tests/test_signal_generator.py
+++ tests/test_signal_generator.py
@@ -641,6 +641,7 @@ def test_momentum_alignment_disables_confirm():
 
     res = rsg.generate_signal(feats_1h, feats_4h, feats_d1)
     assert res['details']['strong_confirm'] is False
+    assert res['details'].get('vote', 0) < 5
 
 
 def test_crowding_factor_and_dynamic_threshold():
@@ -711,4 +712,43 @@ def test_step_exit_with_order_book_flip():
 
     res3 = rsg.generate_signal(f1h, f4h, fd1, order_book_imbalance=-0.3)
     assert res3['signal'] == 0
+
+
+def test_position_size_range_regime():
+    rsg = make_dummy_rsg()
+    rsg.dynamic_weight_update = lambda: rsg.base_weights
+
+    def fake_ai(feats, up, down):
+        if 'atr_pct_1h' in feats:
+            return 0.5
+        return -0.2
+
+    rsg.get_ai_score = fake_ai
+    rsg.get_factor_scores = lambda f, p: {
+        'trend': 0, 'momentum': 0, 'volatility': 0,
+        'volume': 0, 'sentiment': 0, 'funding': 0,
+    }
+    rsg.combine_score = lambda ai, fs, weights=None: ai
+    rsg.dynamic_threshold = lambda *a, **k: 0.1
+    rsg.compute_tp_sl = lambda *a, **k: (0, 0)
+    rsg.models = {'1h': {'up': None, 'down': None},
+                  '4h': {'up': None, 'down': None},
+                  'd1': {'up': None, 'down': None}}
+
+    f1h = {
+        'close': 100,
+        'atr_pct_1h': 0.01,
+        'adx_1h': 10,
+        'funding_rate_1h': 0,
+        'vol_ma_ratio_1h': 1.0,
+    }
+    f4h = {'atr_pct_4h': 0.01, 'adx_4h': 15}
+    fd1 = {'atr_pct_d1': 0.01, 'adx_d1': 20}
+
+    res = rsg.generate_signal(f1h, f4h, fd1,
+                              raw_features_1h=f1h,
+                              raw_features_4h=f4h,
+                              raw_features_d1=fd1)
+    expected = 0.1 + 0.4 * abs(res['details']['score_1h'])
+    assert res['position_size'] == pytest.approx(expected)
 
