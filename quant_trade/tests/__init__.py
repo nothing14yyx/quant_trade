2bb122db014ad2484a4854d5133299f91f9d5100
nothing14yyx <1873888386@qq.com>
2025-06-24 21:18:08 +0800

    Merge branch 'main' of https://github.com/nothing14yyx/quant_trade

--- quant_trade/tests/__init__.py
+++ quant_trade/tests/__init__.py
@@ -2,3 +2,7 @@ import os
 
 # Expose repository-level tests package for internal imports
 __path__ = [os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'tests'))]
+
+
+def test_signal_generator():
+    return None
