313f143ca8f7d3d28816107e4d30ab66b7d7a8c5
nothing14yyx <1873888386@qq.com>
2025-07-25 11:29:17 +0800

    Merge branch 'main' of https://github.com/nothing14yyx/quant_trade

---
+++ quant_trade/utils/export_feature.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from quant_trade.utils.db import load_config, connect_mysql
+from sqlalchemy import text
+
+cfg = load_config()          # 读取 config.yaml
+engine = connect_mysql(cfg)  # 创建数据库连接
+
+n = 50000  # 需要导出的行数
+query = text("SELECT * FROM features ORDER BY open_time DESC LIMIT :n")
+df = pd.read_sql(query, engine, params={"n": n},
+                 parse_dates=["open_time", "close_time"])
+
+df.to_csv("features_recent.csv", index=False)  # 保存为 CSV
