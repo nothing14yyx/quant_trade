a15138e3d1480f6ca720941501369669dff69c56
nothing14yyx <1873888386@qq.com>
2025-07-25 13:01:47 +0800

    Merge branch 'main' of https://github.com/nothing14yyx/quant_trade

--- quant_trade/utils/export_feature.py
+++ quant_trade/utils/export_feature.py
@@ -5,7 +5,7 @@ from sqlalchemy import text
 cfg = load_config()          # 读取 config.yaml
 engine = connect_mysql(cfg)  # 创建数据库连接
 
-n = 50000  # 需要导出的行数
+n = 10000  # 需要导出的行数
 query = text("SELECT * FROM features ORDER BY open_time DESC LIMIT :n")
 df = pd.read_sql(query, engine, params={"n": n},
                  parse_dates=["open_time", "close_time"])
